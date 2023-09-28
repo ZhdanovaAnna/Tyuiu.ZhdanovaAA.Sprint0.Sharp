96e0663356457faf1e8f69831a8474dff730f00e
ZhdanovaAnna <annaone01@mail.ru>
2023-09-28 11:45:44 +0500

    Task3

---
+++ Tyuiu.ZhdanovaAA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZhdanovaAA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(24, DataService.Sum(13, 11));
+        }
+    }
+}
---
+++ Tyuiu.ZhdanovaAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZhdanovaAA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(37, 46));
+            Console.ReadKey();
+        }
+    }
+}
